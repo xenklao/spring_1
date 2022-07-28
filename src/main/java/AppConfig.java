041dfe6310de6cda97a41432db2575111074c443
xenklao <109021940+xenklao@users.noreply.github.com>
2022-07-28 11:35:48 +0500

    Add class Cat,change App, Appcongig

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -17,10 +17,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+
 @Configuration
 public class AppConfig {
 
     @Bean(name="helloworld")
+
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
