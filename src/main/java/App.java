2a1fd883ce36b55f1a7bc81d7cc65017a401c031
xenklao <109021940+xenklao@users.noreply.github.com>
2022-07-28 11:25:39 +0500

    Add class Cat,change App, Appcongig

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,30 @@
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//
+//public class App {
+//    public static void main(String[] args) {
+//        ApplicationContext applicationContext =
+//                new AnnotationConfigApplicationContext(AppConfig.class);
+//        HelloWorld bean =
+//                (HelloWorld) applicationContext.getBean("helloworld");
+//        System.out.println(bean.getMessage());
+//    }
+//}
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Первая пара - " + (bean1 == bean2));
+        System.out.println("Вторая пара - " + (cat1 == cat2));
     }
 }
